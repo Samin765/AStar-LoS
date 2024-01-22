cd2cae208f2ede229375008118002cf466a527da
Lianhang Liu <lianhang@kth.se>
2024-01-23 05:33:27 +0800

    Update CarAI.cs

--- Assets/Scripts/Vehicle/CarAI.cs
+++ Assets/Scripts/Vehicle/CarAI.cs
@@ -13,188 +13,116 @@ namespace UnityStandardAssets.Vehicles.Car
         private CarController m_Car; // the car controller we want to use
         private MapManager mapManager;
         private BoxCollider carCollider;
-        
-        private List<Vector3> path;
-        private List<Vector3> obstacleMap;
+        //define a class for A* search
         public class Node
         {
-            public Vector2Int worldPosition;
+            public Vector3 position;
             public Node parent;
-            public int gCost;
-            public int hCost;
-            public int fCost { get { return gCost + hCost; } }
-
-            public Node(Vector2Int _worldPos, Vector2Int _startPos, Vector2Int _targetPos)
+            public float g;
+            public float h;
+            public float f;
+            public Node(Vector3 pos, Node par, float g, float h)
             {
-                
-                worldPosition = _worldPos;
-                gCost = GetDistance(_startPos, _worldPos );
-                hCost = GetDistance(_worldPos, _targetPos);
+                position = pos;
+                parent = par;
+                this.g = g;
+                this.h = h;
+                f = g + h;
             }
-
-
-            
         }
-        private List<Vector3> FindPath(Vector2Int startWorldPos, Vector2Int targetWorldPos)
+
+
+        public List<Vector3> AStar(Vector3 start, Vector3 goal, Dictionary<Vector2Int, ObstacleMap.Traversability> mapData)
         {
-            // NOTES: We are creating new Nodes in "Neighbour" and we are using these new Node Objects in the open/closed sets. When we try to use equal we are doing object reference. 
-            //        so even if Nodes have same worldPoint they will be treated as different Nodes whhich in turn makes use pick the same Node multiple times thus a infinite loop. 
-            // Neighbour added into OpenSet can be a Node we have already traversed. The algorithm wrongly chooses the node, it does not take the node with the lowest hCost when multiple nodes have same fCost
-            Debug.Log(startWorldPos);
-            Debug.Log(targetWorldPos);
-            Node startNode = new Node(startWorldPos, startWorldPos, targetWorldPos);
-            Node targetNode = new Node(targetWorldPos, startWorldPos, targetWorldPos);
-            Debug.Log(startNode.hCost);
-            Debug.Log(targetNode.hCost);
-            if (targetNode == null || startNode == null) {
-                Debug.Log("Start/Target = Null");
-                return new List<Vector3>();
-            }
-
-            
-            List<Node> openSet = new List<Node>();
-            HashSet<Node> closedSet = new HashSet<Node>();
-            var obstacleMap = mapManager.GetObstacleMap();
-            Dictionary<Vector2Int, ObstacleMap.Traversability> mapData = obstacleMap.traversabilityPerCell;
-            openSet.Add(startNode);
-
-            var j = 0;
-            while (openSet.Count > 0 && j < 4)
+            List<Vector3> path = new List<Vector3>();
+            List<Node> openList = new List<Node>();
+            List<Node> closeList = new List<Node>();
+            Node startNode = new Node(start, null, 0, 0);
+            openList.Add(startNode);
+            while (openList.Count > 0)
             {
-                Node currentNode = openSet[0];
-                Debug.Log("Current Node in OpenSet: " + currentNode.worldPosition);
-                if (currentNode == null){
-                    Debug.Log("CurrentNode == null!");
-                    return new List<Vector3>();
-                }
-
-
-
-                for (int i = 1; i < openSet.Count; i++)
+                Node currentNode = openList[0];
+                for (int i = 1; i < openList.Count; i++)
                 {
-                    if ((openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost) && openSet[i].hCost < currentNode.hCost)
+                    if (openList[i].f < currentNode.f)
                     {
-                        currentNode = openSet[i];
+                        currentNode = openList[i];
                     }
                 }
-
-                openSet.Remove(currentNode);
-                closedSet.Add(currentNode);
-
-                if (currentNode.worldPosition == targetNode.worldPosition)
+                openList.Remove(currentNode);
+                closeList.Add(currentNode);
+                if (Distance(currentNode.position,goal)==0)
                 {
-                    Debug.Log("Target Reached!");
-                    return RetracePath(startNode, targetNode);
-                }
-
-
-                List<Node> neighbours = GetNeighbours(currentNode, startWorldPos, targetWorldPos);
-
-                Debug.Log("GetNeighbours Done!");
-                foreach (Node neighbour in neighbours)
-                {
-                    ObstacleMap.Traversability traversability = mapData[neighbour.worldPosition];
-
-                    if (closedSet.Contains(neighbour) || !(traversability == ObstacleMap.Traversability.Free))
+                    Node temp = currentNode;
+                    while (temp != null)
                     {
+                        path.Add(temp.position);
+                        temp = temp.parent;
+                    }
+                    path.Reverse();
+                    return path;
+                }
+                List<Node> successors = new List<Node>();
+                Vector3[] directions = { new Vector3(1, 0, 0), new Vector3(-1, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 0, -1) };
+                foreach (Vector3 dir in directions)
+                {
+                    Vector3 pos = currentNode.position + dir;
+                    Vector2Int gridPos = new Vector2Int((int)pos.x, (int)pos.z);
+                    if(pos==goal)
+                    {
+                        Node theNode = new Node(pos, currentNode, currentNode.g + 1, Distance(pos, goal));
+                        successors.Add(theNode);
                         continue;
                     }
-
-                    int newCostToNeighbour = currentNode.gCost + GetDistance(currentNode.worldPosition, neighbour.worldPosition);
-                    if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    if (!mapData.ContainsKey(gridPos) || mapData[gridPos] == ObstacleMap.Traversability.Blocked)
                     {
-                        neighbour.gCost = newCostToNeighbour;
-                        neighbour.hCost = GetDistance(neighbour.worldPosition, targetNode.worldPosition);
-                        neighbour.parent = currentNode;
-
-                        if (!openSet.Contains(neighbour))
-                            openSet.Add(neighbour);
+                        int ttt = 0;
+                        continue;
+                    }
+                  
+                    Node newNode = new Node(pos, currentNode, currentNode.g + 1, Distance(pos, goal));
+                    successors.Add(newNode);
+                    
+                }
+                foreach (Node successor in successors)
+                {
+                    //maybe a mistake in juding whether the node is in the closeList,juding only with the position
+                    int flag = 0;
+                    foreach (Node node in closeList)
+                    {
+                        if (node.position == successor.position)
+                        {
+                            flag = 1;
+                            continue;
+                        }
+                    }
+                    foreach (Node node in openList)
+                    {
+                        if (node.position == successor.position)
+                        {
+                           
+                            if (successor.g < node.g)
+                            {
+                                node.g= successor.g;
+                                node.parent = currentNode;
+                                node.f = node.g + node.h;
+                            }
+                            flag = 1;
+                            continue;
+                        }
+                    }
+                    if(flag==0)
+                    {
+                        openList.Add(successor);
                     }
                 }
-
-                j = j + 1;
             }
-            Debug.Log("Did not reach target (end of function)!");
-            return new List<Vector3>();
-        }
-
-        private List<Vector3> RetracePath(Node startNode, Node endNode)
-        {
-            ObstacleMap obstacleMap = mapManager.GetObstacleMap();
-
-            List<Vector3> path = new List<Vector3>();
-            Node currentNode = endNode;
-
-            while (currentNode != startNode)
-            {
-                Vector2Int gridPosition = currentNode.worldPosition;
-
-                Vector3 localPosition = obstacleMap.grid.LocalToWorld(obstacleMap.grid.CellToLocalInterpolated(new Vector3Int(gridPosition.x, 0, gridPosition.y)));
-                Vector3 worldPosition = mapManager.grid.LocalToWorld(localPosition);
-
-                path.Add(worldPosition);
-                currentNode = currentNode.parent;
-            }
-            path.Reverse();
             return path;
         }
-        private static int GetDistance(Vector2Int worldPoisitionA, Vector2Int worldPositionB)
+        //define a function to calculate the distance between two points,mahattan distance
+        public float Distance(Vector3 pos1, Vector3 pos2)
         {
-            int distX = Mathf.Abs(Mathf.RoundToInt(worldPoisitionA.x - worldPositionB.x));
-            int distY = Mathf.Abs(Mathf.RoundToInt(worldPoisitionA.y - worldPositionB.y));
-            //if (distX > distY)
-            //    return 14 * distY + 10 * (distX - distY);
-            //return 14 * distX + 10 * (distY - distX);
-            return Mathf.Max(Mathf.Abs(worldPoisitionA.x - worldPositionB.x), Mathf.Abs(worldPoisitionA.y - worldPositionB.y));
-
-            //return Mathf.RoundToInt(Vector3.Distance(worldPoisitionA, worldPositionB));
-        }
-        private List<Node> GetNeighbours(Node node, Vector2Int startPos, Vector2Int targetPos)
-        {
-            List<Node> neighbours = new List<Node>();
-            var obstacleMap = mapManager.GetObstacleMap();
-            Dictionary<Vector2Int, ObstacleMap.Traversability> mapData = obstacleMap.traversabilityPerCell;
-            //foreach (Vector2Int key in mapData.Keys)
-            //{
-            //Debug.Log(key.ToString());
-            //}
-            for (int x = -1; x <= 1; x++)
-            {
-                for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0)
-                        continue;
-
-
-                    var worldPoint = node.worldPosition + new Vector2Int(x, y);
-                    Vector2Int vector2Int = new Vector2Int(Mathf.RoundToInt(worldPoint.x), Mathf.RoundToInt(worldPoint.y));
-
-                    //Debug.Log("checking Neighbour");
-                    //Debug.Log(vector2Int);
-
-
-                    if (mapData.ContainsKey(vector2Int))
-                    {
-                        //Debug.Log("GridPosition Valid");
-
-                        ObstacleMap.Traversability traversability = mapData[vector2Int];
-
-                        if (traversability == ObstacleMap.Traversability.Free)
-                        {
-                            Node nNode = new Node(vector2Int, startPos, targetPos );
-                            Debug.Log("Added " + vector2Int + " with fCost/hCost : " + nNode.fCost + "/" + nNode.hCost + " as Neighbour to " + node.worldPosition);
-                            neighbours.Add(nNode);
-                        }
-
-
-
-                    }
-
-
-                }
-            }
-
-            return neighbours;
+            return Math.Abs(pos1.x - pos2.x) + Math.Abs(pos1.z - pos2.z);
         }
 
         private void Start()
@@ -224,45 +152,41 @@ namespace UnityStandardAssets.Vehicles.Car
             {
                 Vector2Int gridPos = new Vector2Int(posThreeDim.x, posThreeDim.z);
             }
-
-
-            
-            
             // If you need more details, feel free to check out the ObstacleMap class internals.
 
 
             // Replace the code below that makes a random path
             // ...
 
-            Vector3 goal_pos = mapManager.localGoalPosition;
-
-            //Debug.Log(start_pos);
             Vector3 start_pos = mapManager.localStartPosition;
-
-            var grid_start_pos = obstacleMap.grid.LocalToCell(start_pos);
-            var grid_goal_pos =  obstacleMap.grid.LocalToCell(goal_pos);
+            //make the start_pas has the same y as the goal_pos
             
-            //Debug.Log(grid_start_pos);
-            List<Vector3> my_path = new List<Vector3>();
+            Vector3 goal_pos = mapManager.localGoalPosition;
+            start_pos.y = (float)0.2;
+            goal_pos.y = start_pos.y;
 
 
-            // DU ÄNDRADE ALLT TILL VECTOR2INT, NÄR DU ÄNDRADE DET SÅ BLEV FOR LOOPEN INFINITE ELR NÅT
+            //set the goal near the start
+            //goal_pos.x = start_pos.x -2;
+            //use A* to find the path
+            //set the start and the goal are integers' coordinates
+            start_pos.x = (float)Math.Round(start_pos.x);
+            start_pos.z = (float)Math.Round(start_pos.z);
+            goal_pos.x = (float)Math.Round(goal_pos.x);
+            goal_pos.z = (float)Math.Round(goal_pos.z);
+            //whther the start and the goal are in the map or the blocked area
+            if (!mapData.ContainsKey(new Vector2Int((int)start_pos.x, (int)start_pos.z)) || mapData[new Vector2Int((int)start_pos.x, (int)start_pos.z)] == ObstacleMap.Traversability.Blocked)
+            {
+                Debug.Log("the start is not in the map or the blocked area");
+                return;
+            }
+            //use mapdata in A* to judge whether the point is in the map
 
-            
-            Vector2Int start_vector2Int = new Vector2Int(Mathf.FloorToInt(grid_start_pos.x), Mathf.FloorToInt(grid_start_pos.y));
-            Vector2Int goal_vector2Int = new Vector2Int(Mathf.FloorToInt(grid_goal_pos.x), Mathf.FloorToInt(grid_goal_pos.y));
-
-            Debug.Log(start_vector2Int);
-            my_path = FindPath(start_vector2Int, goal_vector2Int);
-            //Debug.Log(my_path);
-            // Plot your path to see if it makes sense
-            // Note that path can only be seen in "Scene" window, not "Game" window
+            List<Vector3> my_path = AStar(start_pos, goal_pos,mapData);
             Vector3 old_wp = start_pos;
             foreach (var wp in my_path)
             {
-                Debug.Log(wp);
-                Debug.Log("---");
-                Debug.DrawLine(mapManager.grid.LocalToWorld(old_wp), mapManager.grid.LocalToWorld(wp), Color.white, 1000f);
+                Debug.DrawLine(mapManager.grid.LocalToWorld(old_wp), mapManager.grid.LocalToWorld(wp), Color.red, 1000f);
                 old_wp = wp;
             }
         }
@@ -270,20 +194,7 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
-            var obstacleMap = mapManager.GetObstacleMap();
-
-            Vector3 start_pos = mapManager.localStartPosition;
-            Vector3 currentGridPosition = mapManager.grid.WorldToLocal(transform.position);
-            //Vector3 world_pos = mapManager.worldStartPosition;
-
-            Vector3 currentt_pos = mapManager.grid.WorldToLocal(transform.position);
-
-            var grid_start_pos = obstacleMap.grid.LocalToCell(currentt_pos);
-            //Debug.Log(grid_start_pos);
             // How to calculate if a physics collider overlaps another.
-            //Debug.Log(start_pos);
-            //Debug.Log(currentGridPosition);
-
             var exampleObstacle = mapManager.GetObstacleMap().obstacleObjects[0];
 
             var globalPosition = transform.position;
@@ -322,37 +233,14 @@ namespace UnityStandardAssets.Vehicles.Car
 
             // Check and print traversability of currect position
             Vector3 myLocalPosition = mapManager.grid.WorldToLocal(transform.position); // Position of car w.r.p map coordinate origin (not world global)
-            //(obstacleMap.IsLocalPointTraversable(myLocalPosition));
+            var obstacleMap = mapManager.GetObstacleMap();
+            Debug.Log(obstacleMap.IsLocalPointTraversable(myLocalPosition));
 
             // Execute your path here
             // ...
 
             // this is how you control the car
-            m_Car.Move(0f, 1f, 1f, 0f);
-            //FollowPath();
-        }
-
-        private void FollowPath()
-        {
-            //Debug.Log(path)
-            if (path != null && path.Count > 0)
-            {
-                Vector3 nextPoint = path[0];
-                Vector3 directionToNextPoint = (nextPoint - transform.position).normalized;
-
-                float steeringAngle = Vector3.SignedAngle(transform.forward, directionToNextPoint, Vector3.up);
-                steeringAngle = Mathf.Clamp(steeringAngle / 45.0f, -1f, 1f);
-
-                float acceleration = 1.0f;
-                float brake = 0.0f;
-
-                m_Car.Move(steeringAngle, acceleration, brake, 0f);
-
-                if (Vector3.Distance(transform.position, nextPoint) < 1.0f)
-                {
-                    path.RemoveAt(0);
-                }
-            }
+            m_Car.Move(1f, 1f, 1f, 0f);
         }
     }
 }
